d10836f775abe63a9b938b49878ff01e80137b7e
ElkheirImane <102419335+ElkheirImane@users.noreply.github.com>
2022-06-24 17:21:10 +0100

    commit

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validator, Validators} from "@angular/forms";
 import {Customer} from "../model/customer.model";
 import {CustomerService} from "../services/customer.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-new-customer',
@@ -10,7 +11,7 @@ import {CustomerService} from "../services/customer.service";
 })
 export class NewCustomerComponent implements OnInit {
   newCustomerFormGroup! : FormGroup;
-  constructor(private  fb : FormBuilder,private customerService:CustomerService) { }
+  constructor(private  fb : FormBuilder,private customerService:CustomerService, private router:Router) { }
 
   ngOnInit(): void {
     this.newCustomerFormGroup=this.fb.group({
@@ -24,6 +25,8 @@ export class NewCustomerComponent implements OnInit {
     this.customerService.saveCustomer(customer).subscribe({
       next : data =>{
         alert("Customer has been successfully saved!");
+       // this.newCustomerFormGroup.reset();
+        this.router.navigateByUrl("/customers");
 
       },
 
